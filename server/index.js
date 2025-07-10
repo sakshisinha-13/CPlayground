3d603bbafe269afa99b7e1cc60fc3c51621e1034
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-07-10 10:02:13 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://frontend:3000",
-      "https://c-playground.vercel.app"
+      "https://c-playground.vercel.app",
       "http://localhost:3000",
       "http://cplayground.me:3000",
       "http://cplayground.me",
